fb1c437d1b37b2856b27edfdc436ec8a629dc4de
yuyueugene84 <yuyueugene84@gmail.com>
2014-03-08 21:19:34 +0800

    Initial Commit

---
+++ Segmentation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Segmentation
+//
+//  Created by Eugene Chang on 14/3/8.
+//  Copyright (c) 2014年 Eugene Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
